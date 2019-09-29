2995314b822745e9e6d982afda2e375ca7eabdbe
DrGsan <drgsan@yahoo.com>
2019-09-29 16:54:11 +0300

    hw

---
+++ 2.advanced/1.1.Import.Module.Package/application/db/people.py
@@ -0,0 +1,4 @@
+def get_employees():
+    some_text = 'This is some text from people.py'
+    print(some_text)
+    return
---
+++ 2.advanced/1.1.Import.Module.Package/application/salary.py
@@ -0,0 +1,4 @@
+def calculate_salary():
+    some_text = 'This is some text from salary.py'
+    print(some_text)
+    return
