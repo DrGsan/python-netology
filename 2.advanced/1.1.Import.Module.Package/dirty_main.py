01c4dd00896475bf762ee9c2bee429012c76a53e
DrGsan <drgsan@yahoo.com>
2019-09-29 16:42:33 +0300

    hw

---
+++ 2.advanced/1.1.Import.Module.Package/dirty_main.py
@@ -0,0 +1,11 @@
+from application.salary import *
+from application.db.people import *
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    print('-'*50)
+    print('Looks like everything is fine')
+    print('It was dirty_main.py')
+    print('-'*50)
+    print('-'*50)
