db1f124fd7c959dcefbab4a352b9f76d39fe55eb
DrG <drgsan@yahoo.com>
2019-10-15 19:24:01 +0300

    done

--- 3.Django/2.1.site-form-works/credit_calc/app/views.py
+++ 3.Django/2.1.site-form-works/credit_calc/app/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
+# from django.template.context_processors import request
+
+from django.views.generic import TemplateView
 
 from .forms import CalcForm
 
 
-def calc_view(request):
-    template = "app/calc.html"
+class CalcView(TemplateView):
+    template_name = "app/calc.html"
 
-    form = CalcForm
-    context = {
-        'form': form
-    }
+    def get(self, request):
+        common_result = None
+        result = None
+        if request.method == 'GET':
+            form = CalcForm(request.GET)
 
-    return render(request, template, context)
+            if form.is_valid():
+                initial_fee = int(request.GET['initial_fee'])
+                rate = int(request.GET['rate'])
+                months_count = int(request.GET['months_count'])
+                common_result = round((initial_fee + initial_fee * rate) / months_count)
+                result = round(common_result / months_count)
+
+            return render(
+                request,
+                self.template_name,
+                {
+                    'form': form,
+                    'common_result': common_result,
+                    'result': result,
+                },
+            )
