24b121d0e4c495398516c8e0cbd7173bb5f69b3a
DrGsan <drgsan@yahoo.com>
2019-10-18 13:17:17 +0300

    in progress

--- 3.Django/2.1.site-form-works/review/app/views.py
+++ 3.Django/2.1.site-form-works/review/app/views.py
@@ -17,17 +17,33 @@ def product_list_view(request):
 
 
 def product_view(request, pk):
+    is_review_exist = False
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
+    reviews = product.review.all()
 
-    form = ReviewForm
+    if request.method == 'GET':
+        if not request.session['reviewed_products']:
+            request.session['reviewed_products'] = list()
+        if product.id in request.session['reviewed_products']:
+            is_review_exist = True
+
     if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = Review(text=form.cleaned_data['text'], product=product)
+            review.save()
+            request.session.modified = True
+            if product.id not in request.session['reviewed_products']:
+                request.session['reviewed_products'].append(product.id)
+    else:
+        form = ReviewForm()
 
+    print(request.session['reviewed_products'])
     context = {
         'form': form,
-        'product': product
-    }
+        'product': product,
+        'reviews': reviews,
+        'is_review_exist': is_review_exist}
 
     return render(request, template, context)
