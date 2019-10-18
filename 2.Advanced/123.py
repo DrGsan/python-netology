93ffdbd68407607489ead0a25cbeb51d8242c6cb
DrGsan <drgsan@yahoo.com>
2019-10-18 12:17:41 +0300

    in progress

--- 2.Advanced/123.py
+++ 2.Advanced/123.py
@@ -3,3 +3,5 @@ https://github.com/dukov1975/ADPY-4/blob/5dbfed64ddff50839c005d22e9038afa3a36a6f
 https://github.com/alekseykonotop
 
 https://github.com/altovsky/dj-hw/tree/1503583308516f9e2adb370ecea322d97fa55df1/site-form-works
+
+https://github.com/x350/django/tree/c6deb12175cfc5fc44de7d3e1a4a998203470ccc
