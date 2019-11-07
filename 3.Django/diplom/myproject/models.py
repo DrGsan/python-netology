d7846731f88479713f083a6864622f5504dc8dab
DrGsan <drgsan@yahoo.com>
2019-11-07 08:11:08 +0300

    done

---
+++ 3.Django/diplom/myproject/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Phone(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Basket(models.Model):
+    pass
